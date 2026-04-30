2c3dff0a39219099625246739c5a0504ba689a47
jevitapearl <jevitapearl@gmail.com>
2026-04-30 16:35:06 +0530

    Commmenting unused components

--- src/api/uploadCart.ts
+++ src/api/uploadCart.ts
@@ -1,21 +1,21 @@
-import getSKeys from "./getSKeys";
-import { UploadedFile, DocumentConfig } from "../types";
+// import getSKeys from "./getSKeys";
+// import { UploadedFile, DocumentConfig } from "../types";
 
-type CartItem = {
-  file: UploadedFile;
-  config: DocumentConfig;
-};
+// type CartItem = {
+//   file: UploadedFile;
+//   config: DocumentConfig;
+// };
 
-function uploadCart(items: CartItem[]){
-  const data: []= items.map(({file, config}) => {
-    return {
-      file_id: file.id,
-      file_name: file.name,
-      content_type: file.type
-    }
-  })
-  const s_keys = getSKeys(data);
-  return;
-}
+// function uploadCart(items: CartItem[]){
+//   const data: []= items.map(({file, config}) => {
+//     return {
+//       file_id: file.id,
+//       file_name: file.name,
+//       content_type: file.type
+//     }
+//   })
+//   const s_keys = getSKeys(data);
+//   return;
+// }
 
-export default uploadCart;
+// export default uploadCart;
