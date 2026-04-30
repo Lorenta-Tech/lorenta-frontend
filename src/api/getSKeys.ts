bb04413c973648b2d6c8f3a90aa9817ac06db1c0
jevitapearl <jevitapearl@gmail.com>
2026-04-30 17:05:31 +0530

    Commenting unused api function

--- src/api/getSKeys.ts
+++ src/api/getSKeys.ts
@@ -1,21 +1,21 @@
-export default function getSKeys(data: []){
+// export default function getSKeys(data: []){
 
 
-  const url = ""
-  const reqHeaders = new Headers()
+//   const url = ""
+//   const reqHeaders = new Headers()
 
-  fetch(url, {
-    headers: {
+//   fetch(url, {
+//     headers: {
 
-    },
-    credentials: "include",
-    method: "GET",
-    body: JSON.stringify(data)
-  })
-  .then(response => response.json())
-  .then(json =>console.log(json))
-  .catch((err) => console.log(err))
+//     },
+//     credentials: "include",
+//     method: "GET",
+//     body: JSON.stringify(data)
+//   })
+//   .then(response => response.json())
+//   .then(json =>console.log(json))
+//   .catch((err) => console.log(err))
 
   
-  return;
-}
+//   return;
+// }
