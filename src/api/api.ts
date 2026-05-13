f7e1c05d0fa8c1413ebcfddfdf1cebe24b87dda2
suhas-developer07 <sharmasuhas22@gmail.com>
2026-05-13 07:31:07 +0530

    fix: removed slash

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 const BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  "https://kiosk-server-production.duckdns.org/";
+  "https://kiosk-server-production.duckdns.org";
 
 type ApiOptions = RequestInit & {
   auth?: boolean;
